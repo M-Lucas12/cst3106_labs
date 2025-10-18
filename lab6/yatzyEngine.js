a4c0c7cdcbd445d97780c64126ac86f79fa99036
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:40:26 -0400

    Lab 6 Part 4: added sumDice() and countDice() helper methods

--- lab6/yatzyEngine.js
+++ lab6/yatzyEngine.js
@@ -4,4 +4,27 @@ class YatzyEngine {
     constructor() {
         this.scores = {};   // to keep track of category scores
     }
+
+    // Add up all dice values
+    sumDice(dice) {
+        let total = 0;
+        for (let i = 0; i < dice.length; i++) {
+            total += dice[i];
+        }
+        return total;
+    }
+
+    // Count how many times each number appears
+    countDice(dice) {
+        let counts = {};
+        for (let i = 0; i < dice.length; i++) {
+            let value = dice[i];
+            if (counts[value]) {
+                counts[value]++;
+            } else {
+                counts[value] = 1;
+            }
+        }
+        return counts;
+    }
 }
