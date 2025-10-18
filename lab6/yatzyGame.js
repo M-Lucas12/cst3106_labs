a885f606ab9f5d8038caddea7bf8cda08d68d1df
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:43:54 -0400

    Lab 6 Part 8: imported Dice and YatzyEngine into YatzyGame and set up objects

--- lab6/yatzyGame.js
+++ lab6/yatzyGame.js
@@ -1,9 +1,16 @@
 // === Yatzy Game Module ===
+// This file connects the Dice and YatzyEngine modules
+
+import { Dice } from "./dice.js";
+import { YatzyEngine } from "./yatzyEngine.js";
+
 // This class manages the game flow and keeps track of rounds and score
 class YatzyGame {
     constructor() {
         this.round = 1;           // current round number
         this.totalScore = 0;      // player’s total score
         this.currentPlayer = 1;   // player turn tracker
+        this.dice = new Dice(5);  // create 5 dice
+        this.engine = new YatzyEngine(); // scoring engine
     }
 }
