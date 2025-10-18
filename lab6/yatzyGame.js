77b9283fefcd0d6b5c9ce01541a0b82e187462c5
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:42:39 -0400

    Lab 6 Part 7: created YatzyGame class and constructor

---
+++ lab6/yatzyGame.js
@@ -0,0 +1,9 @@
+// === Yatzy Game Module ===
+// This class manages the game flow and keeps track of rounds and score
+class YatzyGame {
+    constructor() {
+        this.round = 1;           // current round number
+        this.totalScore = 0;      // player’s total score
+        this.currentPlayer = 1;   // player turn tracker
+    }
+}
