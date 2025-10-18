639d8d25b43cfd033726d60f4b89a88800d240e5
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:38:06 -0400

    Lab 6 Part 1: created Dice class and constructor

---
+++ lab6/dice.js
@@ -0,0 +1,8 @@
+// === Dice Module ===
+// This class will handle the creation and rolling of dice values
+class Dice {
+    constructor(numDice) {
+        this.numDice = numDice;   // number of dice to roll
+        this.values = [];         // store dice values
+    }
+}
