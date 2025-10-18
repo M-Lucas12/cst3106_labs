acb39f0bf3211081d896f8fd675b0f4216c905d3
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:38:49 -0400

    Lab 6 Part 2: added roll() method to Dice class

--- lab6/dice.js
+++ lab6/dice.js
@@ -5,4 +5,14 @@ class Dice {
         this.numDice = numDice;   // number of dice to roll
         this.values = [];         // store dice values
     }
+
+    // Roll all dice and store the random results
+    roll() {
+        this.values = [];
+        for (let i = 0; i < this.numDice; i++) {
+            let value = Math.floor(Math.random() * 6) + 1;
+            this.values.push(value);
+        }
+        return this.values;
+    }
 }
