77c9bd17e6581c85e3efd57a97c77fd233cebd0b
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:39:46 -0400

    Lab 6 Part 3: created YatzyEngine class and constructor

---
+++ lab6/yatzyEngine.js
@@ -0,0 +1,7 @@
+// === Yatzy Engine Module ===
+// This class will contain all the game rules and scoring functions
+class YatzyEngine {
+    constructor() {
+        this.scores = {};   // to keep track of category scores
+    }
+}
