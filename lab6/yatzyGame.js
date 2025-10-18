df6273685fccaae35d8e0a1bcb68738c52875828
M-Lucas12 <mute0008@algonquinlive.com>
2025-10-18 13:44:30 -0400

    Lab 6 Part 9: added game flow methods to YatzyGame class

--- lab6/yatzyGame.js
+++ lab6/yatzyGame.js
@@ -8,9 +8,33 @@ import { YatzyEngine } from "./yatzyEngine.js";
 class YatzyGame {
     constructor() {
         this.round = 1;           // current round number
-        this.totalScore = 0;      // player’s total score
+        this.totalScore = 0;      // player's total score
         this.currentPlayer = 1;   // player turn tracker
         this.dice = new Dice(5);  // create 5 dice
         this.engine = new YatzyEngine(); // scoring engine
+    }
+
+    // Start a new game
+    startNewGame() {
+        this.round = 1;
+        this.totalScore = 0;
+        console.log("🎲 New Yatzy game started!");
+    }
+
+    // Play one round: roll dice and display values
+    playRound() {
+        let result = this.dice.roll();
+        console.log("Round " + this.round + " roll: " + result);
+    }
+
+    // End current turn and move to next round
+    endTurn() {
+        console.log("End of round " + this.round);
+        this.round++;
+    }
+
+    // End the entire game and show the total score
+    endGame() {
+        console.log("Game Over! Final Score: " + this.totalScore);
     }
 }
